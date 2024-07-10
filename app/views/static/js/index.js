c4e646b29d7928bfb3c7bf4ba400720aed7f744f
coke9603 <compiscastle2@gmail.com>
2024-07-09 23:20:27 -0500

    09/07/2024

--- app/views/static/js/index.js
+++ app/views/static/js/index.js
@@ -1,9 +1,10 @@
 const CITY_QUERY = document.getElementById('cityquery');
 const CITY_LIST = document.getElementById('city_list');
-const CITY_URL = 'api/climate/location?location=';
-const CLIMATE_URL = 'api/climate';
 const CLIMATE_RESULTS = document.getElementById('climate-results');
-const CLIMATE_GRAPH = document.getElementById('climate-graph');
+const CLIMATE_OPT = document.getElementById('climate-opt');
+const CLIMATE_CHART = document.getElementById('climate-chart').getContext('2d');
+const CITY_URL = 'api/location?location=';
+const CLIMATE_URL = 'api/climate';
 
 CITY_QUERY.addEventListener('submit', (event)=>{
     event.preventDefault();
@@ -13,8 +14,17 @@ CITY_QUERY.addEventListener('submit', (event)=>{
         .then(data => show_cities(data))
         .catch(error => console.log(error))  
 })
+    
+function get_climate(lat, lon, info){
+    console.log(CLIMATE_URL + '?lat=' + lat + '&lon=' + lon)
+    fetch(CLIMATE_URL + '?lat=' + lat + '&lon=' + lon)  
+        .then(response => response.json())
+        .then(data => show_climate(data, info))
+        .catch(error => console.log(error))     
+};
 
 function show_cities(data) {
+    console.log(data)
     let city_list = "";
     let climate_graph = ``;
     if (data.length != 0){
@@ -22,61 +32,87 @@ function show_cities(data) {
         city_list += `
         <div class="list-group-item">
             <div class="row justify-content-between">
-                <div class="col-8">` + data[i].name + `, ` + data[i].country + `</div>
+                <div class="col-8">` + data[i].name + `, ` + data[i].state + ` - ` + data[i].country + `</div>
                 <div class="col-2">
-                    <button onclick="get_climate(lat=` + data[i].lat + `,lon=` + data[i].lon + `)">></button>
+                    <button onclick="get_climate(lat=` + data[i].lat + `,lon=` + data[i].lon + `,info='` + data[i].name + `, ` + data[i].state + ` - ` + data[i].country + `')">>></button>
                 </div>
             </div>
         </div>
             `
         };
     } else {
-        city_list += '<label class="list-group-item">No results found</label>'
+        city_list += '<label class="list-group-item">Sin resultados encontrados</label>'
     }
     
     CITY_LIST.innerHTML = city_list;
 };
 
-function get_climate(lat, lon){
-    console.log(CLIMATE_URL + '?lat=' + lat + '&lon=' + lon)
-    fetch(CLIMATE_URL + '?lat=' + lat + '&lon=' + lon)  
-        .then(response => response.json())
-        .then(data => show_climate(data))
-        .catch(error => console.log(error))     
-};
-
-function show_climate(data){
+function show_climate(data, info){
     console.log(data);
     let climate_daily = ``;
-    let climate_graph = ``;
+    let climate_opt = ``;
+    let dates = data['daily']['dates'];
+    let morning = data['daily']['morning'];
+    let day = data['daily']['day'];
+    let evening = data['daily']['evening'];
+    let night = data['daily']['night']; 
     if (Object.keys(data).length != 0){
         climate_daily += `
             <div class="row align-items-start">
                 <h1>` + data["current"]["temp"] + `°C </h1>
                 <p>
+                    ` + info + `<br>
                     Presión atmosférica: ` + data["current"]["pressure"] + `hPa <br>
                     Humedad: ` + data["current"]["humidity"] + `% <br>
                     Nubosidad: ` + data["current"]["clouds"] + `% <br>
                     Índice UV: ` + data["current"]["uvi"] + ` <br>
-                    Visibilidad: ` + data["current"]["visibility"]/8000 + `km <br>
+                    Visibilidad: ` + data["current"]["visibility"]/1000 + `km <br>
                 </p>
             </div>
             `;
-        climate_graph += `
+        climate_opt += `
             <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-primary">Morning</button>
-                <button type="button" class="btn btn-primary">Day</button>
-                <button type="button" class="btn btn-primary">Evening</button>
-                <button type="button" class="btn btn-primary">Night</button>
+                <button type="button" class="btn btn-info">Mañana</button>
+                <button type="button" class="btn btn-primary">Día</button>
+                <button type="button" class="btn btn-success">Tarde</button>
+                <button type="button" class="btn btn-dark">Noche</button>
             </div>
+            <canvas id="climate-chart"></canvas>
             `;
     } else {
         climate_daily += '<label>No results found</label>';
 
     };
     CLIMATE_RESULTS.innerHTML = climate_daily;
-    CLIMATE_GRAPH.innerHTML = climate_graph;
-}
-        
+    CLIMATE_OPT.innerHTML = climate_opt;
+    graph_climate(dates, morning);
+};
 
-        
+function graph_climate(dates, temps){
+    console.log(dates);
+    console.log(temps)
+    let climate_chart = new Chart(CLIMATE_CHART, {
+        type: 'bar',
+        data: {
+            labels: dates,
+            datasets: [{
+                label: 'Temperatura',
+                data: temps,
+                backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+                borderColor: ['rgba(255, 99, 132, 1)'],
+                borderWith: 1.5
+            }],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    })
+}; 
+
+    
+
+    
